d0436fac0edf773b2ce0fdb8681cc410199e1b26
Damian <damian.krzem@gmail.com>
2018-04-22 22:52:28 +0200

    up

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function () {
-$("#but").on("click", function() {
-$.getJSON ("http://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json", function(data){
-    var now = new Date(Date.now());
-    var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds()
-    $("#kurs").text((data.rates[0].mid) + " " + "godzina" + " "+ formatted);      
+    $("#but").on("click", function() {
+        $.getJSON ("http://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json", function(data){
+            var now = new Date(Date.now());
+            var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds()
+            $("#kurs").text((data.rates[0].mid) + " " + "godzina" + " "+ formatted);      
+        });
     });
-});
 });
 
 
