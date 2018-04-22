0c520c8f44d149671e37623731a08dff8427d8c3
Damian <damian.krzem@gmail.com>
2018-04-22 22:53:55 +0200

    up

--- js-NBP/script.js
+++ js-NBP/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $("#but").on("click", function() {
         $.getJSON ("http://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json", function(data){
             var now = new Date(Date.now());
-            var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+            var formatted = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds()
             $("#kurs").text((data.rates[0].mid) + " " + "godzina" + " "+ formatted);      
         });
     });
