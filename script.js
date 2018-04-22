74a9afb2bb959127b83729dd68bb1bdebf45d183
Damian <damian.krzem@gmail.com>
2018-04-22 21:50:33 +0200

    up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
-$.getJSON   ("http://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json", function(data){
+$.getJSON ("http://api.nbp.pl/api/exchangerates/rates/a/eur/?format=json", function(data){
     $("#kurs").text(data.rates[0].mid);
 });
